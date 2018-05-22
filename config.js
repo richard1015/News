785f4e30961ae217ed5d9ce21312cb999aedc970
richard1015 <51844712@qq.com>
2018-05-22 15:06:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,7 @@
  * 小程序配置文件
  */
 
-var host = "https://lightapp.aoji.cn/"
-// var host ="http://192.168.160.162:8001/"
+var host ="http://192.168.160.162:8001/"
 var config = {
   // 下面的地址配合云端 Server 工作
   host
