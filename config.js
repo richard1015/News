5f3ae4e1d1b43692fc0f95332ef72905850ee324
richard <51844712@qq.com>
2018-07-05 09:34:29 +0800

    https域名添加

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // 测试服务器ip
 // var host = "http://192.168.160.163:7001/"
 // 远程线上服务器ip
-var host ="http://66.98.117.14:7001/"
+var host ="https://zhuzhida.vip/"
 var config = {
   // 下面的地址配合云端 Server 工作
   host
